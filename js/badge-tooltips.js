27e41c4e78ce4ab6b8ceb44b31d7f7d7c3dba252
TrePe <TrePe@cb5b18d2-80dd-4a81-9b1c-945e1ee644f9>
2013-02-20 22:06:29 +0000

    Achievement badges update showing correct earned date and "currently at"

--- js/badge-tooltips.js
+++ js/badge-tooltips.js
@@ -1,23 +1,23 @@
-// JavaScript Document
-$(document).ready(function() {
-						 
-	$('.quoteBadgesItem').each(function () {
-		$(this).badgeTooltips({
-			title: $(this).find('.achievementName').text(),
-			content: $(this).find('.achievementDesc').text()//,
-		});
-    });
-	
-});
+/*
+ * Copyright (C) 2012 TopCoder Inc., All Rights Reserved.
+ */
+/**
+ * TopCoder tooltips JQuery plug-in.
+ *
+ * <p>
+ * Version 1.1 (Release Assembly - TopCoder Achievement Utility and Badges Update) Change notes:
+ * - Added logic for "currently @" and proper date of obtaining the achievement.
+ * </p>
+ *
+ * @author TCSASSEMBLER, TrePe
+ * @version 1.1
+ */
 
 ;(function ($) {
-    /* TC tips JQuery plug-in. */
     $.fn.badgeTooltips = function (s) {
         s = $.extend({
-                         title:'',
                          type:'quoteBadgesItemTips',
-                         content:'',
-                         time:''
+                         time:'Not Earned Yet'
                      }, s || {});
         if (!window.hover) {
             window.hover =
@@ -28,27 +28,60 @@ $(document).ready(function() {
         this.s = s;
         this.data('s', s);
         this.hover(
-			function () {
-				var s = $(this).data('s');
-				window.hover.addClass(s.type);
-				$('h3', window.hover).text(s.title);
-				$('p', window.hover).text(s.content);
-				$('span', window.hover).html(s.time);
-				window.hover.show();
-				var offset = $(this).offset();
-				var t = offset.top - window.hover.outerHeight() - 2;
-				var l = offset.left - window.hover.outerWidth() + 42 + $(this).outerWidth() / 2;
-				window.hover.css({
-									 left:l + 'px',
-									 top:t + 'px'
-								 });
-				window.hover.css('display', 'block');
-			},
-			function () {
-				var s = $(this).data('s');
-				window.hover.removeClass(s.type);
-				window.hover.css('display', 'none');
-			}
+            function () {
+                var el = $(this);
+                var s = el.data('s');
+                $('h3', window.hover).text(s.title);
+                $('p', window.hover).text(s.time);
+                window.hover.addClass(s.type);
+                // if this is group badge take values from first one
+                if (typeof s.firstInGroup != 'undefined') {
+                    el = $("."+s.firstInGroup); 
+                    s = el.data('s');
+                }
+                var span = $('span', window.hover);
+                span.html('');
+                if (s.hasCurrentlyAt == 'true') {
+                    if (typeof s.currentlyAtCache == 'undefined') {
+                        s = $.extend({currentlyAtCache: '(retrieving...)'}, s);
+                        el.data('s', s);
+                        span.html('Currently @ (retrieving...)');
+                        $.ajax({
+                            type: "get",
+                            url: '?module=MemberAchievementCurrent&cr=' + userId + '&ruleId=' + s.ruleId,
+                            success: function(data){
+                                if (typeof data.count != 'undefined') {
+                                    s.currentlyAtCache = data.count + ' ' + s.currentlyAtText;
+                                    if (data.count != 1) { // plural
+                                        if (s.currentlyAtText.substr(s.currentlyAtText.length - 3, 2) == 'ch') s.currentlyAtCache += 'e';
+                                        s.currentlyAtCache += 's';
+                                    }
+                                    span.html('Currently @ ' + s.currentlyAtCache);
+                                    el.data('s', s);
+                                } else { // error
+                                    span.html('');
+                                }
+                            }
+                        });
+                    } else {
+                        span.html('Currently @ ' + s.currentlyAtCache);
+                    }
+                }
+                window.hover.show();
+                var offset = $(this).offset();
+                var t = offset.top - window.hover.outerHeight() - 2;
+                var l = offset.left - window.hover.outerWidth() + 42 + $(this).outerWidth() / 2;
+                window.hover.css({
+                                     left:l + 'px',
+                                     top:t + 'px'
+                                 });
+                window.hover.css('display', 'block');
+            },
+            function () {
+                var s = $(this).data('s');
+                window.hover.removeClass(s.type);
+                window.hover.css('display', 'none');
+            }
         );
         return this;
     };
